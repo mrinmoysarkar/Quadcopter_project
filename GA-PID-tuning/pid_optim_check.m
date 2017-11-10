fdce8d1f7eedb06e03b5b9891bda6047d69cb652
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2017-11-10 14:51:24 -0500

    all controllers are working well and flowing the circular trajectory
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- GA-PID-tuning/pid_optim_check.m
+++ GA-PID-tuning/pid_optim_check.m
@@ -5,9 +5,9 @@ close all;
 s = tf('s');
 
 sys = 26.1628/(s^2+14.6211*s);
-kp = 355.056;
-ki = 499.993;
-kd = 24.935;
+kp = 2.231;
+ki = 2.9996;
+kd = 0.5;
 
 controller = kp + ki/s + kd * s; 
 step(feedback(sys*controller,1),5);
