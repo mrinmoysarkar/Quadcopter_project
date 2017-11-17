353d21d867d54718684c7254d682ce5ac4d9805c
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2017-11-16 19:50:19 -0500

    report 2 is added
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- quadcopterSimulinkModeling/plotSimulationOutput.m
+++ quadcopterSimulinkModeling/plotSimulationOutput.m
@@ -23,7 +23,7 @@ x = data.ans.x.Data;
 % plot trajectory in xy-plane
 figure;
 plot(x_d,y_d,'b','linewidth',5);hold on;
-plot(x,y,'ro','linewidth',1);
+plot(x,y,'r--','linewidth',5);
 axis([-1.5 1.5 -1.5 1.5]);
 legend('Reference','Output response');
 xlabel('x in meter');
@@ -32,7 +32,7 @@ ylabel('y in meter')
 %plot trajectory in xyz-co-ordinate
 figure;
 plot3(x_d,y_d,z_d,'b','linewidth',5);hold on;
-plot3(x,y,z,'ro','linewidth',1);
+plot3(x,y,z,'r--','linewidth',5);
 axis([-1.5 1.5 -1.5 1.5 -.1 3]);
 legend('Reference','Output response');
 xlabel('x in meter');
